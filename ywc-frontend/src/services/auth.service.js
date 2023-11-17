a2f0612e6193cbed212a2997f02760fb8f7da7b2
laura_ay <laura.ay1564@gmail.com>
2023-11-16 16:33:32 -0800

    working on auth.service

--- ywc-frontend/src/services/auth.service.js
+++ ywc-frontend/src/services/auth.service.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import http from "../http-common";
 
-const API_URL = "http://localhost:5001/api/auth/";
+// const API_URL = "http://localhost:5000/api/auth/";
 
 const register = (username, email, password) => {
-  return axios.post(API_URL + "signup", {
+  return http.post("/signup", {
     username,
     email,
     password,
