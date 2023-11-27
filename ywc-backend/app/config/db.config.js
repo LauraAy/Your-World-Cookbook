1c0fbbda0184579924f5a18e204a8360d084fadd
laura_ay <laura.ay1564@gmail.com>
2023-11-27 14:24:03 -0800

    updating database config for production

--- ywc-backend/app/config/db.config.js
+++ ywc-backend/app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "sql4pen4e",
+    USER: "lauraay",
+    PASSWORD: "4QLpa44!",
     DB: "cookbookdb",
     dialect: "mysql",
     pool: {
