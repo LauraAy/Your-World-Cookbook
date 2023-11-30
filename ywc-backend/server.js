0fe7a6e0c86df6b941ba8499c8f910042f17e418
laura_ay <laura.ay1564@gmail.com>
2023-11-30 12:32:32 -0800

    trying 5000

--- ywc-backend/server.js
+++ ywc-backend/server.js
@@ -88,7 +88,7 @@ require('./app/routes/recipeSome.routes')(app);
 require('./app/routes/userRecipe.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
