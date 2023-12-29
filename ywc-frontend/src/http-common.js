dd67f024aaa618f56af8d5e413989c2e52cdfea9
laura_ay <laura.ay1564@gmail.com>
2023-12-28 22:53:20 -0800

    updating http-common

--- ywc-frontend/src/http-common.js
+++ ywc-frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://ec2-13-56-232-141.us-west-1.compute.amazonaws.com/api",
+    baseURL: "http://www.yourworldcookbook.com/api",
     headers: {
         "Content-type": "application/json"
     }
