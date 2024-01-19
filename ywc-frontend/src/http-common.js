4b51c6fcc30d5590fe71328ddee17b5b8ba3002f
laura_ay <laura.ay1564@gmail.com>
2024-01-19 14:40:49 -0800

    getting rid of http-common(temporary)

--- ywc-frontend/src/http-common.js
+++ ywc-frontend/src/http-common.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "https://www.yourworldcookbook.com/api",
-    headers: {
-        "Content-type": "application/json"
-    }
-});
+// export default axios.create({
+//     baseURL: "https://www.yourworldcookbook.com/api",
+//     headers: {
+//         "Content-type": "application/json"
+//     }
+// });
 
 // export default axios.create({
 //     baseURL: "http://localhost:3000/api",
