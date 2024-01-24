b243747edc477e747131ab2f7fc0fcb4a64da6c1
laura_ay <laura.ay1564@gmail.com>
2024-01-24 15:18:54 -0800

    reseting for aws

--- ywc-frontend/src/http-common.js
+++ ywc-frontend/src/http-common.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-// export default axios.create({
-//     baseURL: "https://yourworldcookbook.com/api/",
-//     headers: {
-//         "Content-type": "application/json"
-//     }
-// });
-
 export default axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://yourworldcookbook.com/api/",
     headers: {
         "Content-type": "application/json"
     }
- });
+});
+
+// export default axios.create({
+//     baseURL: "http://localhost:3000/api",
+//     headers: {
+//         "Content-type": "application/json"
+//     }
+//  });
